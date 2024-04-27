283b97d2a74c5d981e6c241c33b99bd41eeb8205
Eric Hayter <hayter.eric@gmail.com>
2024-04-27 09:11:25 -0400

    added simple helper function for database

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -1,16 +1,14 @@
 use std::fs;
 use std::path::Path;
 
-pub fn init-db() {
-    if !db_file_exists() {
-        create_db_file()
+pub fn init_db(db_path: &str) {
+    if !Path::new(db_path).exists() {
+        create_db_file(db_path)
     } 
 }
 
-fn create_db_file() {
-    let db_path = get_db_path();
-    let db_dir = Path::new(&db_path).parent().unwrap();
-    
+fn create_db_file(db_path: &str) {
+    let db_dir = Path::new(db_path).parent().unwrap();
 
     // if the parent directory does not exist, create it
     if !db_dir.exists() {
@@ -21,13 +19,8 @@ fn create_db_file() {
     fs::File::create(db_path).unwrap();
 }
 
-fn db_file_exists() -> bool {
-    let db_path = get_db_path();
-    Path::new(&db_path).exists();
-}
-
 // use this with a 
-fn get_db_path(app_handle: tauri::AppHandle) -> String {
+pub fn get_db_path(app_handle: &tauri::AppHandle) -> String {
 //    let home_dir = dirs::home_dir().unwrap();
 //    home_dir.to_str().unwrap().to_string() + 
     let binding = app_handle.path_resolver().app_data_dir().unwrap();
