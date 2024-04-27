b05f871f80281acf1643ce1f93236d017ea1d2f8
Eric Hayter <hayter.eric@gmail.com>
2024-04-27 11:11:39 -0400

    working on fixing main layout (having trouble with sidebar)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,45 +2,25 @@
     import logo from '$lib/images/esponja.svg';
 </script>
 
-<nav class="navbar navbar-expand-lg">
-  <span class="navbar-brand">
-    <img src={logo} alt="" width="48" height="48" class="d-inline-block align-text-top">
-    esponja
-  </span>
-
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
+<nav>
+    <div class="nav-wrapper green">
+        <span class="brand-logo center">esponja</span>
+    </div>
 </nav>
 
-<slot />
+<div class="row">
+
+    <div class="col s12 m3 l2 light-green">
+        <ul>
+            <p href="#" class="white-text text-darken-3">phrases</p>
+            <p href="#">phrases</p>
+            <p href="#">phrases</p>
+        </ul>
+    </div>
+
+    <div class="col s12 m8 l9">
+        <slot />
+    </div>
+</div>
+
+
